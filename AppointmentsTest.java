ba5fdf8e13a47aa5237324a9fc8aa910adeedb42
Kim Ji Soo <kjisu97@hanmail.net>
2018-12-20 12:38:52 +0900

    Update AppointmentsTest.java

--- AppointmentsTest.java
+++ AppointmentsTest.java
@@ -1,25 +1,23 @@
 import static org.junit.Assert.*;
-
 import java.util.Vector;
-
 import org.junit.Test;
 
-class AppointmentsTest {
+public class AppointmentsTest {
 
 	@Test
-	void CreateAppointmentTest() {
+	public void test() {
 		Appointments appointments = new Appointments();
 		assertTrue(appointments.CreateAppointment(20181224, "Jisoo", "SMWU"));
+
 	}
-	
 	@Test
-	void DeleteAppointmentTest() {
-		int Appointmentsize;
-		Appointments appointments = new Appointments();
-		Vector<Word> v= new Vector<Word>();
-		Appointmentsize=v.size();
-		appointments.DeleteAppointment(20181224, "Jisoo", "SMWU");
-		assertEquals(Appointmentsize-1,v.size());
+	public void testCollectionAdd() {
+	   Appointments appointments = new Appointments();
+	   Vector<Word> v= new Vector<Word>();
+	   int sizeAppointment=v.size();
+	   appointments.ViewAppointment();
+	   assertEquals(sizeAppointment, v.size());
 	}
+
 
 }
